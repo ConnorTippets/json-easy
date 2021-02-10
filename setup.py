cd7583d96b55d6d778d048cde74ff02555e296b8
Con <50535379+ConnorTippets@users.noreply.github.com>
2021-02-10 00:30:57 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     url='https://github.com/ConnorTippets/json-easy/',
-    author='ConnorTippets',
+    author='meiscool466',
     license='MIT',
     packages=["json_easy"],
     python_requires=">=3.8",
